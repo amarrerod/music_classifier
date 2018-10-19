c547bd576d5d1c969c6b86b276ee11f692d06535
marreA <alemarrerodiaz@gmail.com>
2018-10-19 17:11:25 +0100

    Clase de datos funcionando

--- musicclassifier/dataset.py
+++ musicclassifier/dataset.py
@@ -1,5 +1,19 @@
+import os
 import numpy as np
-
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+from sklearn.utils import shuffle
 class Dataset():
-    def __init__(self):
-        pass
+    def __init__(self, file='./data/music_data.csv'):
+        self.data = pd.read_csv('./data/music_data.csv')
+        self.data = shuffle(self.data)
+        self.features = list(self.data.columns)
+        self.features.remove('filename')
+    def plot_features(self):
+        # for f in self.features[0:-1]:
+        #     figure, axis = plt.subplots(figsize=(20, 10))
+        #     axis.tick_params(axis='both', which='major', labelsize=16)
+        #     sns.barplot(data=self.data, x='label', y=f)
+        # plt.show()
+        return True
--- tests/tests_dataset.py
+++ tests/tests_dataset.py
@@ -7,3 +7,13 @@ class Test_Dataset(unittest.TestCase):
 
     def test_dataset_exists(self):
         self.assertIsNotNone(self.dataset)
+    def test_data_exists(self):
+        self.assertIsNotNone(self.dataset.data)
+    def test_data_has_features(self):
+        self.assertIsNotNone(self.dataset.features)
+    def test_data_features_are_29(self):
+        self.assertEqual(29, len(self.dataset.features))
+    def test_data_show_plots_True(self):
+        self.assertTrue(self.dataset.plot_features())
+    def test_data_rows_are_1001(self):
+        self.assertTrue(1001, len(self.dataset.data))
